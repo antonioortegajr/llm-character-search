b106eda4301c74454476f468bf00bf16c6124dd9
Antonio Ortega <antonioortega@Mac.hsd1.or.comcast.net>
2025-01-16 19:12:10 -0800

    add api call to marvel api

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # llm-character-search
 using llms to find characters
+
+## About
+
+This repo was started because I am a Dr. Doom fan. He may not always have his own title but is often in the description. Using Marvels API can pass the description to an LLM to guess if Doom is in the comic.
+
+## Use
+
+python3 -m venv venv
+
+source venv/bin/activate
+
+. venv/bin/activate
+
+python main.py
--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from prompt_llm import prompt_llama
 
-result = prompt_llama("What is the capital of France?")
+# will hook up to marvel when their api let's me back in
+# https://github.com/antonioortegajr/llm-character-search/issues/1
+data = ""
+
+#prompt for dr. doom
+prompt = f"""
+given the data below, is the marvel character Dr. Doom in any of these comic books?
+
+Data: 
+{{data}}
+"""
+
+result = prompt_llama("")
 print(result)
 # Or specify a different model:
 result = prompt_llama("What is the capital of France?", model="llama3")
